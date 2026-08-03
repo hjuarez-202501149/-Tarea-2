e108f9c78174eb975765e3f078afa9e208909fd1
hjuarez-202501149 <juarezh629@gmail.com>
2026-08-03 13:06:18 -0600

    Creamos la estructura inicial del programa

---
+++ src/actividad2/Actividad2.java
@@ -0,0 +1,33 @@
+package actividad2;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Hans Juárez 
+ */
+
+public class Actividad2 {
+
+    static Scanner entrada = new Scanner(System.in);
+
+    static int totalDatos = 5;
+    static double media = 0;
+    static char respuesta = 'S';
+    static boolean programaActivo = true;
+
+    public static void main(String[] args) {
+
+        /*
+         * Programa para trabajar con arreglos y matrices.
+         */
+
+        System.out.println("================================");
+        System.out.println(" ANALIZADOR DE NUMEROS ");
+        System.out.println("================================");
+        System.out.println("Proyecto iniciado correctamente.");
+
+    }
+
+}
